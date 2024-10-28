d13f8ed8ed80bde7b5db72efa7aa665442f447fd
LEEEUNCHEOL96 <dmscjf1020@naver.com>
2024-10-28 11:25:25 +0900

    DTO 수정/ 다건조회

--- src/main/java/com/example/rest_api/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/rest_api/article/controller/ApiV1ArticleController.java
@@ -1,9 +1,14 @@
 package com.example.rest_api.article.controller;
 
+import com.example.rest_api.article.dto.ArticleDTO;
+import com.example.rest_api.article.entity.Article;
 import com.example.rest_api.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController // 문자를 데이터 형태로 넘겨줌
 @RequiredArgsConstructor
@@ -12,8 +17,14 @@ public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("") // 다건조회
-    public String list(){
-        return "목록";
+    public List<ArticleDTO> list(){
+        List<ArticleDTO> articleDTOList = new ArrayList<>();
+
+        articleDTOList.add(new ArticleDTO(new Article("제목1","내용1")));
+        articleDTOList.add(new ArticleDTO(new Article("제목2","내용2")));
+        articleDTOList.add(new ArticleDTO(new Article("제목3","내용3")));
+
+        return articleDTOList;
     }
 
     @GetExchange("/{id}") //단건조회
--- src/main/java/com/example/rest_api/article/dto/ArticleDTO.java
+++ src/main/java/com/example/rest_api/article/dto/ArticleDTO.java
@@ -1,23 +1,31 @@
 package com.example.rest_api.article.dto;
 
+import com.example.rest_api.article.entity.Article;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class ArticleDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private final Long id;
 
-    private String subject;
+    private final String subject;
 
-    private String content;
+    private final String content;
 
-    private LocalDateTime createdDate;
+    private final LocalDateTime createdDate;
 
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
+    private final LocalDateTime modifiedDate;
+
+    public ArticleDTO(Article article){
+        this.id= article.getId();
+        this.subject=article.getSubject();
+        this.content=article.getContent();
+        this.createdDate=article.getCreatedDate();
+        this.modifiedDate=article.getModifiedDate();
+    }
 }

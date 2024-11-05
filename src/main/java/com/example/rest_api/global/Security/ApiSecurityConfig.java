5238914f6a40e7e4d432cd263489ad475697697e
LEEEUNCHEOL96 <dmscjf1020@naver.com>
2024-11-05 12:49:20 +0900

    jwt filter

--- src/main/java/com/example/rest_api/global/Security/ApiSecurityConfig.java
+++ src/main/java/com/example/rest_api/global/Security/ApiSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +26,7 @@ public class ApiSecurityConfig {
                                 .requestMatchers(HttpMethod.GET, "/api/*/articles/*").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/*/members/login").permitAll() // 로그인은 누구나 가능, post 요청만 허용
                                 .requestMatchers(HttpMethod.GET, "/api/*/members/me").permitAll() // 로그인은 누구나 가능, post 요청만 허용
+                                .requestMatchers(HttpMethod.POST, "/api/*/members/logout").permitAll() // 로그아웃은 누구나 가능, post 요청만 허용
                                 .anyRequest().authenticated()
                 )
                 .csrf(
@@ -39,8 +41,11 @@ public class ApiSecurityConfig {
                 ) // 폼 로그인 방식 끄기
                 .sessionManagement(
                         sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(
+                        jwtAuthorizationFilter, //엑세스 토큰을 이용한 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
                 );
-        ;
         return http.build();
     }
 }

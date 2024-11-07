ba7d215048df118e5277ab85ce19cfe070618879
LEEEUNCHEOL96 <dmscjf1020@naver.com>
2024-11-07 17:30:18 +0900

    vsc CORS Errors 해결 & articleList 출력

--- frontapp/src/pages/ArticleList.jsx
+++ frontapp/src/pages/ArticleList.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react'
+
 export default function ArticleList() {
-    return <>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-        </ul>
-    </>
+    const [articleList, setArticleList] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8090/api/v1/articles')
+            .then((res) => res.json)
+            .then((res) => {
+                setArticleList(res.data.articleList)
+            })
+    }, [])
+
+    return (
+        <>
+            <ul>
+                {articleList.map((article) => (
+                    <li key={article.id}>
+                        {article.id} / {article.subject} / {article.content} / {article.author}
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
